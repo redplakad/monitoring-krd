5806448fc994584c4a6f841ccaae55d3586dbd04
Galih Setiabudi <galihstbd@gmail.com>
2025-05-23 08:42:15 +0700

    fix ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +28,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         
     }
 }
