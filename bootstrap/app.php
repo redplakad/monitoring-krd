6643c9c59f964984741d4a54b6c00e7970faa9ac
Galih Setiabudi <galihstbd@gmail.com>
2025-06-03 09:19:48 +0700

    edit middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,10 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->trustProxies(at: '*');
         $middleware->web([
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class, // <--- ganti ini
         ]);
         
